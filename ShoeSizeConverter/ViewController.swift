7ff48f7bc08ac7393f4cb70f6584d574b76c9600
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-06 21:37:14 -0500

    Initial Commit

---
+++ ShoeSizeConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoeSizeConverter
+//
+//  Created by HUGE | Isaiah Belle on 1/6/15.
+//  Copyright (c) 2015 IsaiahBelleDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
