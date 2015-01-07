b3d64f50f3533612cd9515479d1f68952048e1fe
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-01-06 22:41:20 -0500

    Initial Working Version
    Initial Working Version

--- ShoeSizeConverter/ViewController.swift
+++ ShoeSizeConverter/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mensShoeSizeTF: UITextField!
+    @IBOutlet weak var convertedLabel: UILabel!
+    @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var warningLabel: UILabel!
+    let CONV_CONSTANT = 9
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let fromSize = mensShoeSizeTF.text
+        let numberFromSizeTxt = fromSize.toInt()
+        if (numberFromSizeTxt != nil) {
+            var integerFromTextField =  numberFromSizeTxt!
+            integerFromTextField*=CONV_CONSTANT
+            convertedLabel.hidden=false
+            let stringWithUpdatedShoeSize = "\(integerFromTextField)"
+            convertedLabel.text = stringWithUpdatedShoeSize
+        }else{
+            convertedLabel.hidden=true
+            convertedLabel.text = ""
 
-
+        }
+        mensShoeSizeTF.resignFirstResponder()
+    }
+    
 }
 
